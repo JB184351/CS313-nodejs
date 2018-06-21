66dbad5f6076be051a22033a904ecf17ace8d5b2
Justin Bengtson <justinbengtson@icloud.com>
2018-06-21 16:01:37 -0600

    Testing nodejs website

--- ta09.js
+++ ta09.js
@@ -3,25 +3,12 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-var num1 = parseInt(process.argv[2]);
-var op = process.argv[3];
-var num2 = parseInt(process.argv[4]);
-var result;
-
-
-app.get('/math', function (req, res) {
-  result = eval(num1 + op + num2);
-  res.render('pages/index', {});
-});
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-/*express()
+express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/mailForm', (req, res) => res.render('pages/mailForm'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-*/
+
 
