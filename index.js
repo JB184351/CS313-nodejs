608f0bc48ab39eab70e4a8ac49a1a2d57da0e461
Justin Bengtson <justinbengtson@icloud.com>
2018-06-23 21:12:25 -0600

    PUshing to github

--- index.js
+++ index.js
@@ -3,12 +3,40 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/mailForm'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/mailForm'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+  app.get('/getRate', (req, res) => {
 
+	let rate = getRate(req.query.name, res.query.value);
+
+	var postage = {name: req.query.name, weight: req.query.value, rate: rate };
+	//var myJSON = JSON.stringify(postage);
+
+	res.render('pages/rates', postage);
+
+  });
+
+  function getRate(name, value){
+
+  	if(req.query.name = 'Stamped'){
+  		rate = res.query.value;
+  	}
+
+  	else if(req.query.name = 'Metered'){
+  		rate = res.query.value;
+  	}
+
+  	else if(req.query.name = 'Envelopes'){
+  		rate = res.query.value;
+  	}
+
+  	else if(req.query.name = 'First-Class'){
+  		rate = res.query.value;
+  	}
+
+  }
 
